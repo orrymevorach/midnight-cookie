119cc9baf9cec8535cfe0564a55db18e93fc327c
orrymevorach <38357766+orrymevorach@users.noreply.github.com>
2022-12-23 10:48:43 -0500

    removing animation

--- components/nav/mobile-nav/mobile-nav.jsx
+++ components/nav/mobile-nav/mobile-nav.jsx
@@ -3,14 +3,14 @@ import styles from './mobile-nav.module.scss';
 import NavLinks from '../nav-links';
 
 export default function MobileNav({ navData, pathname, setIsOpen, isOpen }) {
-  useEffect(() => {
-    const body = document.getElementsByTagName('body')[0];
-    if (isOpen) {
-      body.style.overflow = 'hidden';
-    } else {
-      body.style.overflow = 'visible';
-    }
-  }, [isOpen]);
+  // useEffect(() => {
+  //   const body = document.getElementsByTagName('body')[0];
+  //   if (isOpen) {
+  //     body.style.overflow = 'hidden';
+  //   } else {
+  //     body.style.overflow = 'visible';
+  //   }
+  // }, [isOpen]);
   return (
     <>
       <nav className={styles.mobileNav}>

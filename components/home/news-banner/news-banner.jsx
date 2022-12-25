0107838ea956ba01839bfcd544375632318b3046
orrymevorach <38357766+orrymevorach@users.noreply.github.com>
2022-12-25 12:20:55 -0500

    link news banner to media page

--- components/home/news-banner/news-banner.jsx
+++ components/home/news-banner/news-banner.jsx
@@ -3,45 +3,48 @@ import Heading from 'components/shared/heading';
 import Image from 'next/image';
 import { AnimationOnScroll } from 'react-animation-on-scroll';
 import animations from 'mevo-components/animations/animations.module.scss';
+import Link from 'next/link';
 
 export default function NewsBanner() {
   return (
     <AnimationOnScroll animateIn={animations.fadeUp}>
       <Heading text="As Seen On" />
-      <div className={styles.row}>
-        <Image
-          src="/wp-content/uploads/2021/12/blogto.png"
-          width={200}
-          height={200}
-          alt="BlogTO Logo"
-        />
-        <Image
-          src="/wp-content/uploads/2022/12/tastetoronto.jpeg"
-          width={200}
-          height={200}
-          alt="Taste Toronto Logo"
-        />
-        <Image
-          src="/wp-content/uploads/2022/12/star.png"
-          width={200}
-          height={200}
-          alt="Toronto Star Logo"
-        />
-      </div>
-      <div className={styles.row}>
-        <Image
-          src="/wp-content/uploads/2021/08/daily-hive.jpeg"
-          width={200}
-          height={200}
-          alt="Daily Hive Logo"
-        />
-        <Image
-          src="/wp-content/uploads/2022/12/streets.png"
-          width={200}
-          height={200}
-          alt="Streets Logo"
-        />
-      </div>
+      <Link href="/media">
+        <div className={styles.row}>
+          <Image
+            src="/wp-content/uploads/2021/12/blogto.png"
+            width={200}
+            height={200}
+            alt="BlogTO Logo"
+          />
+          <Image
+            src="/wp-content/uploads/2022/12/tastetoronto.jpeg"
+            width={200}
+            height={200}
+            alt="Taste Toronto Logo"
+          />
+          <Image
+            src="/wp-content/uploads/2022/12/star.png"
+            width={200}
+            height={200}
+            alt="Toronto Star Logo"
+          />
+        </div>
+        <div className={styles.row}>
+          <Image
+            src="/wp-content/uploads/2021/08/daily-hive.jpeg"
+            width={200}
+            height={200}
+            alt="Daily Hive Logo"
+          />
+          <Image
+            src="/wp-content/uploads/2022/12/streets.png"
+            width={200}
+            height={200}
+            alt="Streets Logo"
+          />
+        </div>
+      </Link>
     </AnimationOnScroll>
   );
 }

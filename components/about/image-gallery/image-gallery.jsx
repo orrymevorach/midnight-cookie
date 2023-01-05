0df65075ad6e10c4c780e74bc83f4ad81fe6d06d
orrymevorach <38357766+orrymevorach@users.noreply.github.com>
2023-01-05 13:55:46 -0500

    fix images from wp admin

--- components/about/image-gallery/image-gallery.jsx
+++ components/about/image-gallery/image-gallery.jsx
@@ -1,12 +1,11 @@
 import styles from './image-gallery.module.scss';
-import Image from 'next/image';
 import { AnimationOnScroll } from 'react-animation-on-scroll';
 import animations from 'mevo-components/animations/animations.module.scss';
 
 export default function ImageGallery() {
   return (
     <div className={styles.imageContainer}>
-      <Image
+      <img
         width="240"
         height="375"
         src="/wp-content/uploads/2021/10/mid-1.jpg"
@@ -14,7 +13,7 @@ export default function ImageGallery() {
         alt=""
       />
       <div className={styles.rightColumn}>
-        <Image
+        <img
           width="340"
           height="210"
           src="/wp-content/uploads/2021/10/mid2.jpg"
@@ -22,7 +21,7 @@ export default function ImageGallery() {
           alt=""
         />
         <AnimationOnScroll animateIn={animations.fadeUp}>
-          <Image
+          <img
             width="340"
             height="335"
             src="/wp-content/uploads/2022/10/MidnightCookie_Hero.jpg"
--- components/home/reviews/reviews.jsx
+++ components/home/reviews/reviews.jsx
@@ -7,63 +7,63 @@ import { useWindowSize } from 'hooks';
 const reviewData = [
   {
     name: 'Dave M.',
-    src: 'https://www.midnightcookie.ca/wp-content/uploads/2021/10/unnamed-2.png',
     review: 'Absolutely amazing! Delivered warm out of the oven! Delicious!',
   },
   {
     name: 'Rayshelle S.',
-    src: 'https://www.midnightcookie.ca/wp-content/uploads/2021/10/ray.png',
     review:
       'Absolutely amazing product with a TON of options. The customer service was phenomenal I would definitely recommend 10/10',
   },
   {
     name: 'R. M.',
-    src: 'https://www.midnightcookie.ca/wp-content/uploads/2021/10/unnamed-1.png',
     review:
       'Literally amazing. The perfect balance of sweetness, chewiness and moisture. 10/10 would buy again.',
   },
 ];
-export default function Reviews() {
-  const StarIcon = () => (
-    <FontAwesomeIcon
-      icon={faStar}
-      color="#ffcc00"
-      className={styles.starIcon}
-    />
+
+const StarIcon = () => (
+  <FontAwesomeIcon icon={faStar} color="#ffcc00" className={styles.starIcon} />
+);
+
+const Review = ({ name, review }) => {
+  return (
+    <div className={styles.review} key={name}>
+      <div className={styles.row}>
+        <div className={styles.imageContainer}>
+          <div className={styles.quoteIcon}>
+            <FontAwesomeIcon icon={faQuoteLeft} color="white" />
+          </div>
+        </div>
+        <p className={styles.name}>{name}</p>
+        <div>
+          <StarIcon />
+          <StarIcon />
+          <StarIcon />
+          <StarIcon />
+          <StarIcon />
+        </div>
+      </div>
+      <p className={styles.reviewText}>"{review}"</p>
+    </div>
   );
+};
+
+export default function Reviews() {
   const { isTablet, isMobile } = useWindowSize();
-  const removeReviews = reviewData => {
+  const removeReviewsForScreenSize = reviewData => {
     if (isTablet) return reviewData.slice(1);
     if (isMobile) return reviewData.slice(2);
     return reviewData;
   };
 
+  const reviews = removeReviewsForScreenSize(reviewData);
+
   return (
     <div className={styles.reviews}>
       <Heading text="What Our Customers Say" />
       <div className={styles.reviewsContainer}>
-        {removeReviews(reviewData).map(({ name, src, review }) => {
-          return (
-            <div className={styles.review} key={name}>
-              <div className={styles.row}>
-                <div className={styles.imageContainer}>
-                  <img src={src} alt="" />
-                  <div className={styles.quoteIcon}>
-                    <FontAwesomeIcon icon={faQuoteLeft} color="white" />
-                  </div>
-                </div>
-                <p className={styles.name}>{name}</p>
-                <div>
-                  <StarIcon />
-                  <StarIcon />
-                  <StarIcon />
-                  <StarIcon />
-                  <StarIcon />
-                </div>
-              </div>
-              <p className={styles.reviewText}>"{review}"</p>
-            </div>
-          );
+        {reviews.map(({ name, review }) => {
+          return <Review name={name} review={review} key={name} />;
         })}
       </div>
     </div>

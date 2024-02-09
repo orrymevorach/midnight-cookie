2448b7ac54041eb28d915fda267e0a57d8146ed1
orrymevorach <38357766+orrymevorach@users.noreply.github.com>
2024-02-09 17:11:46 -0500

    cookie gallery mobile

--- pages/about-us.js
+++ pages/about-us.js
@@ -5,12 +5,15 @@ import Paragraph from 'components/about/paragraph/paragraph';
 import LogoRow from 'components/about/logo-row/logo-row';
 import CookiePieces from 'components/about/cookie-pieces/cookie-pieces';
 import CookiePiecesV2 from 'components/about/cookie-pieces-v2/cookie-pieces-v2';
+import { useWindowSize } from 'hooks';
 
 export default function AboutPage(pageProps) {
+  const { isMobile } = useWindowSize();
   return (
     <Layout {...pageProps}>
       <LogoRow />
       <LogoRow />
+      {isMobile && <LogoRow />}
       <CookiePieces />
       <main>
         <Paragraph />

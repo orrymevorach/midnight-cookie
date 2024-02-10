2d74383faa43dbe65c89e524ec26e674d8d52a0e
orrymevorach <38357766+orrymevorach@users.noreply.github.com>
2024-02-10 10:54:36 -0500

    fix cookie v2 gallery

--- components/home/cookie-gallery-v2/cookie-tile-v2/cookie-tile-v2.jsx
+++ components/home/cookie-gallery-v2/cookie-tile-v2/cookie-tile-v2.jsx
@@ -1,17 +1,28 @@
+import { useWindowSize } from 'hooks';
 import styles from './cookie-tile-v2.module.scss';
+import Image from 'next/image';
 
 export default function CookieTile({
   cookieData: {
     title,
-    image: { url },
+    image: { url, width, height },
   },
 }) {
+  const { isMobile } = useWindowSize();
+  const style = isMobile ? { backgroundImage: `url(${url})` } : {};
   return (
     <div className={styles.container}>
-      <div
-        className={styles.cookieTile}
-        style={{ backgroundImage: `url(${url})` }}
-      ></div>
+      <div className={styles.cookieTile} style={style}>
+        {!isMobile && (
+          <Image
+            src={url}
+            width={width}
+            height={height}
+            className={styles.image}
+            alt=""
+          />
+        )}
+      </div>
       <div>
         {title.split(' ').map((word, index) => (
           <p key={`${word}-${index}`} className={styles.title}>

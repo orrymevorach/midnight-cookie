abc95af714da7ce4d7e0f86717965aae250d9758
orrymevorach <38357766+orrymevorach@users.noreply.github.com>
2026-01-16 13:39:00 -0500

    google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -2,10 +2,13 @@ import '../styles/globals.css';
 import PageNotFound from './404';
 import '@fortawesome/fontawesome-svg-core/styles.css';
 import { config } from '@fortawesome/fontawesome-svg-core';
+import { useGoogleAnalytics } from 'lib/google-analytics-lib';
 config.autoAddCss = false;
 
 export default function App({ Component, pageProps }) {
   const { isPagePublished, isPreview } = pageProps;
+  useGoogleAnalytics();
+
   if (!isPagePublished && !isPreview) return <PageNotFound />;
   return (
     <>
